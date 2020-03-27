dda0ebef22e25886451a51968ca91c470c027a3f
Caio Dottori <caio.dottori@starkbank.com>
2020-03-26 21:31:57 -0300

    refactored utils request with got lib

--- starkbank/utils/request.js
+++ starkbank/utils/request.js
@@ -1,5 +1,6 @@
 const Ecdsa = require('@starkbank/ecdsa');
-const https = require('https');
+const got = require('got');
+const pjson = require('./package.json');
 
 class Response {
     constructor(status, content) {
@@ -13,60 +14,52 @@ class Response {
 }
 
 exports.apiFetch = function (user, path, method = 'GET', payload = null,
-                             query = null, version = 'v2', callback) {
+                             query = null, version = 'v2') {
     let hostname = {
         'production': 'https://api.starkbank.com/' + version,
         'sandbox': 'https://sandbox.api.starkbank.com/' + version,
         'development': 'https://development.api.starkbank.com/' + version,
     }[user.environment];
+
+    let options = {
+        method: method,
+        json: false
+    };
+
+    let url = hostname + path;
     if (query) {
-        let queryString = '';
-        queryString = '?';
+        let queryString = '?';
         for (let key in query) {
             queryString += key + '=' + query[key];
         }
-        path += queryString;
-    }
-    url = hostname + path;
-    let accessTime = Math.round((new Date()).getTime() / 1000);
-    let body = null;
-    let message = user.accessId() + ':' + accessTime + ':';
-    if (payload && (method === 'POST' || method === 'PUT' || method === 'PATCH')) {
-        body = JSON.stringify(payload);
-        message += body;
+        url += queryString;
     }
 
-    headers = {
+    let accessTime = Math.round((new Date()).getTime() / 1000);
+    let message = user.accessId() + ':' + accessTime + ':';
+
+    if (payload && (method === 'POST' || method === 'PUT' || method === 'PATCH')) {
+        let body = JSON.stringify(payload);
+        message += body;
+        options['body'] = body
+    }
+
+    options['headers'] = {
         'Access-Id': user.accessId(),
         'Access-Time': accessTime,
         'Access-Signature': Ecdsa.Ecdsa.sign(message, user.privateKey()).toBase64(),
         'Content-Type': 'application/json',
-        'User-Agent': 'Node-' + process.versions['node'] + '-SDK-2.0.0',
+        'User-Agent': 'Node-' + process.versions['node'] + '-SDK-' + pjson.version,
     };
-    console.log('Method:', method);
-    console.log('Message:', message);
-    let options = {
-        'headers': headers,
-        'method': method,
-    };
-    console.log(url);
-    let request = https.request(url, options, (response) => {
-        // Add timeout.
-        request.setTimeout(10000, function () {
-            request.abort();
-        });
-        let body = '';
-        response.on('data', function (chunk) {
-            body += chunk;
-        });
-        response.on('end', function () {
-            let resp = new Response(response.statusCode, body);
-            console.log(-1);
-            console.log(resp.content);
-            console.log(0);
-            callback(resp);
-        });
-    });
-    request.end();
-    return request;
+
+    let response = null;
+    (async () => {
+        try {
+            response = await got(url, options);
+        } catch (error) {
+            console.log(error.response.body);
+        }
+    })();
+
+    return new Response(response.status, response.content);
 };
