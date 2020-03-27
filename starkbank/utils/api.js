f714b63c521271ca3f4f393e07201759cf8c274d
Dalton Felipe de Menezes <dalton.menezes@starkbank.com>
2020-03-27 18:28:23 -0300

    Add name functions

--- starkbank/utils/api.js
+++ starkbank/utils/api.js
@@ -1,6 +1,19 @@
 const decamelize = require('decamelize');
 
 
-exports.endpoint = function (resource) {
-    return decamelize(resource, '-').replace('-log', '/log');
+exports.endpoint = function (resource, keepDash = false) {
+    let decamelized = decamelize(resource, '-');
+    if (keepDash) {
+        return decamelized;
+    }
+    return decamelized.replace('-log', '/log');
+};
+
+exports.lastName = function (resource) {
+    let splitString = decamelize(resource, '-').split('-')
+    return splitString[splitString.length-1];
+};
+
+exports.lastPlural = function (resource) {
+    return `${exports.lastName(resource, true)}s`;
 };
