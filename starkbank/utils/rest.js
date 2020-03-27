838bce494611f64bcfcd9628c5ab31a1a53ce74e
Dalton Felipe de Menezes <dalton.menezes@starkbank.com>
2020-03-27 18:29:15 -0300

    Add post, getId and delete rest functions

--- starkbank/utils/rest.js
+++ starkbank/utils/rest.js
@@ -18,25 +18,45 @@ exports.getList = function* (resource, cursor = null, limit = 100, user = null) 
     } while (!cursor || (limit && limit <= 0));
 };
 
-exports.post = function (resource, entities, user = null) {
-
+exports.post = async function (resource, entities, user = null) {
+    let entity = new resource;
+    let names = api.lastPlural(entity.constructor.name);
+    let endpoint = `${api.endpoint(entity.constructor.name)}`;
+    for (let i in entities) {
+        Object.keys(entities[i]).forEach(key => entities[i][key] === null && delete entities[i][key]);
+    }
+    let payload = {};
+    payload[names] = entities;
+    console.log(payload);
+    let response = await apiFetch(user, `/${endpoint}`, 'POST', payload, null, 'v2');
+    let list = JSON.parse(response.content)[api.lastName(names)];
+    let newList = [];
+    for (let entity of list) {
+        newList.push(Object.assign(new resource, entity));
+    }
+    return newList;
 };
 
 exports.getPdf = function (resource, entities, user = null) {
 
 };
 
-exports.getId = function (resource, id, user = null, callback) {
-    let endpoint = `${api.endpoint(resource)}/${id}`;
-    let json;
-    apiFetch(user, `/${endpoint}`, 'GET', null, null, 'v2', (response) => {
-        json = JSON.parse(response.content);
-    });
-    callback(json);
+exports.getId = async function (resource, id, user = null, callback) {
+    let entity = new resource;
+    let name = entity.constructor.name;
+    let endpoint = `${api.endpoint(name)}/${id}`;
+    let response = await apiFetch(user, `/${endpoint}`, 'GET', null, null, 'v2');
+    let returnEntity = JSON.parse(response.content)[api.lastName(name)];
+    return Object.assign(entity, returnEntity);
 };
 
-exports.delete = function (resource, entities, user = null) {
-
+exports.deleteId = async function (resource, id, user = null) {
+    let entity = new resource;
+    let name = entity.constructor.name;
+    let endpoint = `${api.endpoint(name)}/${id}`;
+    let response = await apiFetch(user, `/${endpoint}`, 'DELETE', null, null, 'v2');
+    let returnEntity = JSON.parse(response.content)[api.lastName(name)];
+    return Object.assign(entity, returnEntity);
 };
 
 exports.postSingle = function (resource, entities, user = null) {
