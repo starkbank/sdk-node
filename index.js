386ed9306bd9668375e84497e1d9dfb45e8e82b5
Caio Dottori <caio.dottori@starkbank.com>
2020-05-04 13:17:20 -0300

    version 0.3.0

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.version = '0.2.0';
+exports.version = '0.3.0';
 exports.cache = {};
 exports.user = null
 
--- tests/utils/user.js
+++ tests/utils/user.js
@@ -4,14 +4,15 @@ exports.exampleProject = new starkbank.Project(
     {
         environment: 'sandbox',
         id: '5656565656565656',
-        privateKey: `-----BEGIN EC PARAMETERS-----
-BgUrgQQACg==
------END EC PARAMETERS-----
------BEGIN EC PRIVATE KEY-----
-MHQCAQEEIMCwW74H6egQkTiz87WDvLNm7fK/cA+ctA2vg/bbHx3woAcGBSuBBAAK
-oUQDQgAE0iaeEHEgr3oTbCfh8U2L+r7zoaeOX964xaAnND5jATGpD/tHec6Oe9U1
-IF16ZoTVt1FzZ8WkYQ3XomRD4HS13A==
------END EC PRIVATE KEY-----
-`
+        privateKey: `
+        -----BEGIN EC PARAMETERS-----
+        BgUrgQQACg==
+        -----END EC PARAMETERS-----
+        -----BEGIN EC PRIVATE KEY-----
+        MHQCAQEEIMCwW74H6egQkTiz87WDvLNm7fK/cA+ctA2vg/bbHx3woAcGBSuBBAAK
+        oUQDQgAE0iaeEHEgr3oTbCfh8U2L+r7zoaeOX964xaAnND5jATGpD/tHec6Oe9U1
+        IF16ZoTVt1FzZ8WkYQ3XomRD4HS13A==
+        -----END EC PRIVATE KEY-----
+        `
     }
 );
