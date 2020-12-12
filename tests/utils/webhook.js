08fcd05e52b82ba39d722157f1caec60d3277f94
vidalstark <matheus.vidal@starkbank.com>
2020-12-12 01:51:07 -0300

    Update Webhook doc string

--- tests/utils/webhook.js
+++ tests/utils/webhook.js
@@ -4,6 +4,6 @@ const starkbank = require('../../index.js');
 exports.generateExampleWebhook = function () {
     return new starkbank.Webhook({
         url: 'https://webhook.site/a10b29fc-45cf-4a09-b743-b7dff8c9eea5',
-        subscriptions: ['transfer', 'boleto', 'boleto-payment', 'utility-payment'],
+        subscriptions: ['transfer', 'boleto', 'boleto-payment', 'boleto-holmes', 'brcode-payment', 'utility-payment', 'deposit', 'invoice'],
     });
 };
