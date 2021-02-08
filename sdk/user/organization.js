a5a6602c1d0e668df1e66d4c56615bff9b7b04d7
Caio Dottori <caio.dottori@starkbank.com>
2021-02-08 18:12:22 -0300

    Update README to include self-service organization key upload

--- sdk/user/organization.js
+++ sdk/user/organization.js
@@ -14,7 +14,7 @@ class Organization extends User {
     * of your organization can register or change the Organization credentials.
     * All requests to the Stark Bank API must be authenticated via an SDK user,
     * which must have been previously created at the Stark Bank website
-    * [https://sandbox.web.starkbank.com] or [https://web.starkbank.com]
+    * [https://web.sandbox.starkbank.com] or [https://web.starkbank.com]
     * before you can use it in this SDK. Organizations may be passed as the user parameter on
     * each request or may be defined as the default user at the start (See README).
     * If you are accessing a specific Workspace using Organization credentials, you should
