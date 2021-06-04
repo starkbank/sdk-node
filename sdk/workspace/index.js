8361e905da2b1a3dc8d2eefb2ff0394426ff712a
Jonatan Rinckus <jonatan@loop3.com.br>
2021-06-04 14:38:49 -0300

    add update workspace

--- sdk/workspace/index.js
+++ sdk/workspace/index.js
@@ -2,5 +2,6 @@ const workspace = require('./workspace');
 
 exports.create = workspace.create;
 exports.get = workspace.get;
+exports.update = workspace.update;
 exports.query = workspace.query;
 exports.Workspace = workspace.Workspace;
